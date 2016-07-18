b4943ecc980b8ca2fd83c643f51e782d821b1512
gustavoelizalde <gustavoelizalde@gmail.com>
2016-07-18 19:58:23 -0400

    Lógica a nivel de de modelo

--- test/models/inventory_test.rb
+++ test/models/inventory_test.rb
@@ -8,11 +8,18 @@ class InventoryTest < ActiveSupport::TestCase
   def setup
   	@product_1 = inventories(:one)
   	@product_2 = inventories(:two)
+  	@product_3 = Inventory.new(serial: 3, name: "Test Product", wheel: 10, description: "Test tamaño rueda", category_id: 1)
   end
 
   test "Serial number can not be repeated" do
   	# Descomentando esta linea, el test falla, ya que se repiten los Serials, y aparece el mensaje de error
   	#@product_2.serial = 1
   	assert_not_equal @product_1.serial, @product_2.serial, "Serials numbers can not be repeated"
+  end
+
+  test "Wheel size should be grater than 0" do
+  	@product_3.wheel = 0
+  	@product_3.save
+  	assert_not @product_3.valid?, "El tamaño de la rueda debe ser mayor que 0"
   end
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+  	@user = users(:one)
+  end
+
+  test "User Name can not be blank" do
+  	@user.name = ""
+  	@user.save
+  	assert_not @user.valid?, "El Usuario no puede tener un nombre en blanco"
+  end
+end
