493a20fbdaa6eac617720be3d7ab5d0722239b0e
gustavoelizalde <gustavoelizalde@gmail.com>
2016-07-18 21:59:05 -0400

    Interfaz gráfica (2 ptos) INCOMPLETA

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719012425) do
+ActiveRecord::Schema.define(version: 20160719015050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,12 @@ ActiveRecord::Schema.define(version: 20160719012425) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "work_id"
   end
 
   add_index "inventories", ["category_id"], name: "index_inventories_on_category_id", using: :btree
   add_index "inventories", ["serial"], name: "index_inventories_on_serial", unique: true, using: :btree
+  add_index "inventories", ["work_id"], name: "index_inventories_on_work_id", using: :btree
 
   create_table "registers", force: :cascade do |t|
     t.text     "description"
@@ -60,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160719012425) do
   add_index "works", ["user_id"], name: "index_works_on_user_id", using: :btree
 
   add_foreign_key "inventories", "categories"
+  add_foreign_key "inventories", "works"
   add_foreign_key "works", "inventories"
   add_foreign_key "works", "users"
 end
