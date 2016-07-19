43e5f27b669249c5c380e6804a02e74c5552de53
gustavoelizalde <gustavoelizalde@gmail.com>
2016-07-18 21:22:49 -0400

    Cascadas (2 ptos) El Borrado del Item que borre el Work no está funcionando

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ActiveRecord::Base
   belongs_to :user
-  has_one :inventory
+  has_one :inventory, dependent: :destroy
   validates :user, presence: true
 
   scope :last_5, ->{order('created_at DESC').limit(5)}
